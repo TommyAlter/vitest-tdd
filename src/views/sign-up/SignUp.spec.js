cfe0663970d0318d546cb711691a36b80f8fb225
TOMMYALTER\MyPC <vlockhacker@gmail.com>
2024-02-19 05:40:27 +0700

    8. Sign Up Form

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -7,4 +7,45 @@ describe('Sign Up', () => {
     const header = screen.getByRole('heading', { name: 'Sign Up' })
     expect(header).toBeInTheDocument()
   })
+
+  it('has username input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Username')).toBeInTheDocument()
+  })
+
+  it('has email input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Email')).toBeInTheDocument()
+  })
+
+  it('has password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toBeInTheDocument()
+  })
+
+  it('has password type for password input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password')).toHaveAttribute('type', 'password')
+  })
+
+  it('has password confirmation input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Confirmation')).toBeInTheDocument()
+  })
+
+  it('has password type for password confirmation input', () => {
+    render(SignUp)
+    expect(screen.getByLabelText('Password Confirmation')).toHaveAttribute('type', 'password')
+  })
+
+  it('has Sign Up button', () => {
+    render(SignUp)
+    const button = screen.getByRole('button', { name: 'Sign Up' })
+    expect(button).toBeInTheDocument()
+  })
+
+  it('disable the button initially', () => {
+    render(SignUp)
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeDisabled()
+  })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,3 +1,23 @@
 <template>
   <h1>Sign Up</h1>
+  <div>
+    <label for="username">Username</label>
+    <input id="username" />
+  </div>
+
+  <div>
+    <label for="email">Email</label>
+    <input id="email" />
+  </div>
+
+  <div>
+    <label for="password">Password</label>
+    <input id="password" type="password" />
+  </div>
+
+  <div>
+    <label for="password-confirm">Password Confirmation</label>
+    <input id="password-confirm" type="password" />
+  </div>
+  <button disabled>Sign Up</button>
 </template>
