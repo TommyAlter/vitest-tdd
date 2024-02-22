e05dbfa0384c09bb0b6f88bb8ef1e6808fa1a30b
TOMMYALTER\MyPC <vlockhacker@gmail.com>
2024-02-23 04:38:42 +0700

    11. API Request

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -2,12 +2,12 @@
   <h1>Sign Up</h1>
   <div>
     <label for="username">Username</label>
-    <input id="username" />
+    <input id="username" v-model="username" />
   </div>
 
   <div>
     <label for="email">Email</label>
-    <input id="email" />
+    <input id="email" v-model="email" />
   </div>
 
   <div>
@@ -19,13 +19,24 @@
     <label for="password-confirm">Password Confirmation</label>
     <input id="password-confirm" type="password" v-model="passwordConfirm" />
   </div>
-  <button :disabled="isDisabled">Sign Up</button>
+  <button :disabled="isDisabled" @click="submit">Sign Up</button>
 </template>
 
 <script setup>
+import axios from 'axios'
 import { ref, computed } from 'vue'
+const username = ref('')
+const email = ref('')
 const password = ref('')
 const passwordConfirm = ref('')
+
+const submit = () => {
+  axios.post('/api/v1/users', {
+    username: username.value,
+    email: email.value,
+    password: password.value
+  })
+}
 
 const isDisabled = computed(() =>
   password.value || passwordConfirm.value ? password.value !== passwordConfirm.value : true
