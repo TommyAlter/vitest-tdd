81542f7b52fff6e6751c94f7583c7f4bb079913e
TOMMYALTER\MyPC <vlockhacker@gmail.com>
2024-03-06 04:52:26 +0700

    14. Event Modifier

--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,25 +1,27 @@
 <template>
-  <h1>Sign Up</h1>
-  <div>
-    <label for="username">Username</label>
-    <input id="username" v-model="formState.username" />
-  </div>
+  <form @submit.prevent="submit">
+    <h1>Sign Up</h1>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" v-model="formState.username" />
+    </div>
 
-  <div>
-    <label for="email">Email</label>
-    <input id="email" v-model="formState.email" />
-  </div>
+    <div>
+      <label for="email">Email</label>
+      <input id="email" v-model="formState.email" />
+    </div>
 
-  <div>
-    <label for="password">Password</label>
-    <input id="password" type="password" v-model="formState.password" />
-  </div>
+    <div>
+      <label for="password">Password</label>
+      <input id="password" type="password" v-model="formState.password" />
+    </div>
 
-  <div>
-    <label for="password-confirm">Password Confirmation</label>
-    <input id="password-confirm" type="password" v-model="formState.passwordConfirm" />
-  </div>
-  <button :disabled="isDisabled" @click="submit">Sign Up</button>
+    <div>
+      <label for="password-confirm">Password Confirmation</label>
+      <input id="password-confirm" type="password" v-model="formState.passwordConfirm" />
+    </div>
+    <button :disabled="isDisabled">Sign Up</button>
+  </form>
 </template>
 
 <script setup>
