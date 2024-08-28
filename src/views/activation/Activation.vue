5b5978ba1e282e0bc8a629ff854a6a689f3d4229
TOMMYALTER\MyPC <vlockhacker@gmail.com>
2024-08-29 06:00:28 +0700

    78. Custom composable

--- src/views/activation/Activation.vue
+++ src/views/activation/Activation.vue
@@ -1,36 +1,15 @@
 <template>
   <div data-testid="activation-page">
-    <Alert v-if="status === 'fail'" variant="danger">{{ errorMessage }}</Alert>
-    <Alert v-if="status === 'success'">{{ successMessage }}</Alert>
+    <Alert v-if="status === 'fail'" variant="danger">{{ error }}</Alert>
+    <Alert v-if="status === 'success'">{{ data.message }}</Alert>
     <Alert variant="secondary" center v-if="status === 'loading'">
       <Spinner size="normal" />
     </Alert>
   </div>
 </template>
 <script setup>
-import { ref, watchEffect } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { useRoute } from 'vue-router'
 import { Spinner, Alert } from '@/components'
 import { activate } from './api'
-const { t } = useI18n()
-const route = useRoute()
-const errorMessage = ref()
-const successMessage = ref()
-const status = ref('')
-watchEffect(async () => {
-  status.value = 'loading'
-  try {
-    const response = await activate(route.params.token)
-    successMessage.value = response.data.message
-    status.value = 'success'
-  } catch (apiError) {
-    if (apiError.response?.data?.message) {
-      errorMessage.value = apiError.response.data.message
-    } else {
-      errorMessage.value = t('genericError')
-    }
-    status.value = 'fail'
-  }
-})
+import useRouteParamApiRequest from '@/shared/useRouteParamApiRequest'
+const { status, data, error } = useRouteParamApiRequest(activate, 'token')
 </script>
